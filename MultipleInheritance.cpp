28ae37f532a11f2d2204ab4e87499923d40dbdfc
Sai Immani <saichowdaryimmani@gmail.com>
2025-02-10 13:58:43 +0530

    Create MultipleInheritance.cpp

---
+++ MultipleInheritance.cpp
@@ -0,0 +1,18 @@
+class A {
+public:
+    void showA() { cout << "Class A" << endl; }
+};
+
+class B {
+public:
+    void showB() { cout << "Class B" << endl; }
+};
+
+class C : public A, public B {};
+
+int main() {
+    C obj;
+    obj.showA();
+    obj.showB();
+    return 0;
+}
